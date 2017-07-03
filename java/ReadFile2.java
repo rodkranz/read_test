3bc7948df1d7638b080b471ec098f59266994974
Geraldo Massahud <geraldo.santos@olx.com>
2017-07-03 09:43:12 +0100

    no buffered version

--- java/ReadFile2.java
+++ java/ReadFile2.java
@@ -1,9 +1,8 @@
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,22 +17,22 @@ import java.io.InputStreamReader;
 public class ReadFile2 {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("../data.json"));
-        char[] buf = new char[64*1024];
+        FileInputStream is = new FileInputStream("../data.json");
+        byte[] buf = new byte[64*1024];
         long sz = 0;
         long n = 0;
         int chunks = 0;
 
         System.out.println("Started Reading json file.");
         long time = System.currentTimeMillis();
-        while ((n = reader.read(buf)) >= 0) {
-           sz += n;
-           chunks++;
+        while ((n = is.read(buf)) >= 0) {
+            sz += n;
+            chunks++;
         }
         System.out.println(System.currentTimeMillis()-time);
-        System.out.printf("Bytes: %d Chunks: %d\n", sz, chunks);
 
-        reader.close();
+        System.out.printf("Bytes: %d Chunks: %d\n", sz, chunks);
+        is.close();
     }
 
 }
